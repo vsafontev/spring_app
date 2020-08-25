7e505f866b0e93375028191e5bfc2526b146e913
vsafontev <vladislavsafontev@gmail.com>
2020-08-25 10:38:55 +0300

    commit 3

---
+++ src/main/java/ru/safonty/springcourse/TestSpring.java
@@ -0,0 +1,13 @@
+package ru.safonty.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
